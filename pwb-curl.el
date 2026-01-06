3939003f461c7533fdccfc7e6de862fe382261ca
github <83626280+pxel8063@users.noreply.github.com>
2026-01-06 23:22:47 +0900

    format

--- pwb-curl.el
+++ pwb-curl.el
@@ -5,7 +5,10 @@
 	(anthropic-version "anthropic-version: 2023-06-01")
 	(application-json "content-type: application/json")
 	(payload
-	 (json-serialize '(:model "claude-haiku-4-5" :max_tokens 1000 :system "" :messages [(:role "user" :content "hello")]))))
+	 (json-serialize (list :model "claude-haiku-4-5"
+			       :max_tokens 1000
+			       :system ""
+			       :messages [(:role "user" :content "hello")]))))
     (with-temp-buffer
       (call-process "curl" nil t nil host "-s"
 		    "-H" api-key

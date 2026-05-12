f039cc269b94f9cf364a173f1f92296e06462cff
github <83626280+pxel8063@users.noreply.github.com>
2026-05-12 19:24:41 +0900

    typos

--- test/pwb-test.el
+++ test/pwb-test.el
@@ -73,10 +73,10 @@
 			  (cons 'id "msg_01F1rvRpZWutMkCnaUYFjLai")
 			  (cons 'type "message")
 			  (cons 'role "assistant")
-			     :content [(:type "text" :text "Hello! How can I help you today?")]
-			   (cons 'stop_reason "end_turn")
-			   (cons 'stop_sequence 'null)
-			   (cons 'usage (list (cons 'input_tokens 9) (cons 'cache_creation_input_tokens 0) (cons 'cache_read_input_tokens 0) (cons 'cache_creation (list (cons 'ephemeral_5m_input_tokens 0) (cons 'ephemeral_1h_input_tokens 0))) (cons 'output_tokens 12) (cons 'service_tier "standard"))))))))
+			  (cons 'content [((type . "text") (text . "Hello! How can I help you today?"))])
+			  (cons 'stop_reason "end_turn")
+			  (cons 'stop_sequence 'null)
+			  (cons 'usage (list (cons 'input_tokens 9) (cons 'cache_creation_input_tokens 0) (cons 'cache_read_input_tokens 0) (cons 'cache_creation (list (cons 'ephemeral_5m_input_tokens 0) (cons 'ephemeral_1h_input_tokens 0))) (cons 'output_tokens 12) (cons 'service_tier "standard"))))))))
 
 (provide 'pwb-test)
 

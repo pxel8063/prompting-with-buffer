26c21387959f7d66d6fe455b1c568558b4b7ccfc
github <83626280+pxel8063@users.noreply.github.com>
2026-05-23 16:47:55 +0900

    version bump to 0.0.11

--- pwb.el
+++ pwb.el
@@ -3,7 +3,7 @@
 ;; Copyright (C)   2026 pxel8063
 
 ;; Author:     pxel8063 <pxel8063@gmail.com>
-;; Version:    0.0.10
+;; Version:    0.0.11
 ;; Keywords:   lisp
 ;; Package-Requires: ((emacs "29.1"))
 ;; URL:        https://github.com/pxel8063/prompting-with-buffer

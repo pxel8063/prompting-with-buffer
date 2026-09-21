b1a35d543de24f09e80f81c982f5478509af1707
github <83626280+pxel8063@users.noreply.github.com>
2026-09-22 07:10:19 +0900

    change function name pwb-get-assitant-param
    * pwb.el (pwb-get-assistant-param): change function name, to more
    appropriate name

--- pwb.el
+++ pwb.el
@@ -876,7 +876,7 @@ FILE-ID is obtained from Files API."
                 (pwb-file-source file-id))))
 
 ;;; Response API
-(defun pwb-get-messages (response)
+(defun pwb-get-assistant-param (response)
   "Get array message param from RESPONSE.
 Return value is the same shape as that of `pwb-array-message-param'."
   (vector (seq-filter #'(lambda (x) (or (eq 'role (car x))
--- test/pwb-test.el
+++ test/pwb-test.el
@@ -435,8 +435,8 @@
                            (content . [((type . "text")
                                         (text . "Hello, Claude!"))]))])))))
 
-(ert-deftest pwb-get-messages-test ()
-  (should (equal (pwb-get-messages
+(ert-deftest pwb-get-assistant-param-test ()
+  (should (equal (pwb-get-assistant-param
 		  (list (cons 'model "claude-haiku-4-5-20251001")
 			(cons 'id "msg_01F1rvRpZWutMkCnaUYFjLai")
 			(cons 'type "message")
@@ -447,6 +447,7 @@
 			(cons 'usage (list (cons 'input_tokens 9) (cons 'cache_creation_input_tokens 0) (cons 'cache_read_input_tokens 0) (cons 'cache_creation (list (cons 'ephemeral_5m_input_tokens 0) (cons 'ephemeral_1h_input_tokens 0))) (cons 'output_tokens 12) (cons 'service_tier "standard")))))
                  [((role . "assistant")
                    (content . [((type . "text") (text . "Hello! How can I help you today?"))]))])))
+
 
 (provide 'pwb-test)
 
